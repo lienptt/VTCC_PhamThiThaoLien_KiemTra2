06deecb79fa637075ac5846b229814a22836d32a
lienptt <lienptt.test@gmail.com>
2024-06-13 21:04:16 +0700

    Change class name

---
+++ src/test/java/com/lienptt/KiemTra2/testcases/ProductDetailTest.java
@@ -0,0 +1,31 @@
+package com.lienptt.KiemTra2.testcases;
+
+import com.lienptt.KiemTra2.pages.LoginPage;
+import com.lienptt.KiemTra2.pages.ProductPage;
+import com.lienptt.common.BaseTest;
+import com.lienptt.dataproviders.DataProviderFactory;
+import com.lienptt.helpers.CaptureHelper;
+import org.testng.annotations.Test;
+
+public class ProductDetailTest extends BaseTest {
+
+    LoginPage loginPage;
+    ProductPage productPage;
+
+    @Test(dataProvider = "data_provider_login", dataProviderClass = DataProviderFactory.class)
+    public void searchProduct(String email, String password) {
+        loginPage = new LoginPage();
+        loginPage.login(email, password);
+        loginPage.verifyLoginSuccess();
+
+        productPage = new ProductPage();
+        productPage.redirectToAllProductsTab();
+        productPage.assertRedirectAllProductsTabSuccess();
+
+        productPage.inputSearch();
+        productPage.clickViewProductDetail();
+        productPage.clickCloseCookiePopup();
+        productPage.verifyProductDetail();
+        CaptureHelper.captureScreenshot("Product Detail screen");
+    }
+}
